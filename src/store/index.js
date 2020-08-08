ec41302e7cb00e7a061079d7f65138d92a76f1a3
João Inhani <joaoinhani@MacBook-Pro-de-Joao.local>
2020-08-08 08:29:24 -0300

    Adjust General

--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,8 @@ import cartaoReducer from './reducers/actionCartaoReducer';
 import categoriaReducer from './reducers/actionCategoriaReducer';
 import visaoReducer from './reducers/actionVisaoReducer';
 import IslogedReducer from './reducers/isLogedReducer';
-<<<<<<< HEAD
 import receitaprevistaReducer from './reducers/actionreceitaPrevistaReducer';
-=======
 import despesaprevistaReducer from './reducers/actionDespesaPrevistaReducer';
->>>>>>> 782eeb1bcf6036f419215c66e2991b2844664822
 
 const reducers = combineReducers({
     conta: contaReducer,
@@ -17,11 +14,8 @@ const reducers = combineReducers({
     cartao: cartaoReducer,
     visao: visaoReducer,
     categoria: categoriaReducer,
-<<<<<<< HEAD
     receitaprevista: receitaprevistaReducer,
-=======
     despesaprevista: despesaprevistaReducer,
->>>>>>> 782eeb1bcf6036f419215c66e2991b2844664822
 })
 
 export default createStore(reducers)
